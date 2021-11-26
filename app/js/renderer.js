161133c9d899fe4f708332b2681a2a7d56d05a57
DamataVeloper <damatadeveloper@gmail.com>
2021-11-26 16:37:43 -0300

    abrir nova janela apartir de um botao (ipcMain / ipcRenderer)

--- app/js/renderer.js
+++ app/js/renderer.js
@@ -1,1 +1,8 @@
-console.log('ola eu estou na janela do browser');
+//inter process comuniction
+const { ipcRenderer } = require('electron');
+const linkSobre =  document.querySelector('#link-sobre');
+
+linkSobre.addEventListener('click', () => {
+    console.log('oioioioioioi')
+    ipcRenderer.send('abrir-janela-sobre')
+});
--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 // app -> ciclo de vida do app
 // BrowserWindow -> cria a janela do app
 
-
-
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain} = require('electron');
 //const { BrowserWindow } = require ('electron');
 
 app.on('ready', () => {
     console.log("Aplicacao iniciada");
     let mainWindow = new BrowserWindow({
         width: 600,
-        height: 400
-    });
+        height: 400,
 
+        webPreferences: {
+            devTools: false,
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+
+
+    });    
     //mainWindow.loadURL('https://cursos.dankicode.com/');
-    mainWindow.loadURL(`file://${__dirname}/app/index.html`)
+    mainWindow.loadURL(`file://${__dirname}/app/index.html`)    
 });
 
 app.on('window-all-closed', () =>{
     app.quit();
 });
+
+ipcMain.on('abrir-janela-sobre', () =>{
+    let sobreWindow = new BrowserWindow({
+        width: 300,
+        height: 300
+        
+    });
+    console.log("Aplicacao Sobre");
+    sobreWindow.loadURL(`file://${__dirname}/app/sobre.html`);
+})
 
